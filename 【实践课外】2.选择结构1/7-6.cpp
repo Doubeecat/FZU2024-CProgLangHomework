454ec687998a294b7d36e57ab92f0c7c20443fb2
Doubeecat <1359446223@qq.com>
2024-10-19 10:34:52 +0800

    逆天东西 哥换个精度薄纱你

--- 【实践课外】2.选择结构1/7-6.cpp
+++ 【实践课外】2.选择结构1/7-6.cpp
@@ -1,80 +1,48 @@
-/*
-Undo the destiny.
-*/
-#include <algorithm>
-#include <iostream>
-#include <iomanip>
-#include <cassert>
-#include <cstring>
-#include <cstdio>
-#include <cctype>
-#include <vector>
-#include <cmath>
-#include <queue>
-using namespace std;
-#define ll long long
-#define FO(x) {freopen(#x".in","r",stdin);freopen(#x".out","w",stdout);}
-#define pii pair<int,int>
-#define mp make_pair
+#include <stdio.h>
+#include <math.h>
+const long double eps = 1e-15;
+struct poi {
+    long double x,y;
+}p[3];
 
-const ll mod = 998244353;
-const double eps = 1e-10;
-const int N = 110;
-
-int sgn(double x) {
+int sgn(long double x) {
     if (fabs(x) < eps) return 0;
     return x > 0 ? 1 : -1;
 }
-
-struct point {
-    int x,y;
-    friend point operator - (const point &a,const point &b) {
-        point c;
-        c.x = a.x - b.x;
-        c.y = a.y - b.y;
-        return c;
-    }
-    friend double operator * (const point &a,const point &b) {
-        double ans = (double)a.x * b.x + a.y * b.y;
-        return ans;
-    }
-    friend double operator ^ (const point &a,const point &b) {
-        return (double)a.x * b.y - b.x * a.y;
-    }
-}P[N];
-
-double len(point a) {
-    return sqrt(a*a);
-}
-
-double area(point a,point b,point c) {
-    return (double)fabs((b-a) ^ (c-a)) / 2.0;
-}
-
-bool judge(point a,point b,point c) {
-    return !sgn((b-a) ^ (c-b));
-}
-int n;
  
-void solve() {
-    cin >> P[1].x >> P[1].y >> P[2].x >> P[2].y >> P[3].x >> P[3].y;
-    cin >> n;
-    for (int i = 1;i <= n;++i) cin >> P[i].x >> P[i].y;
-    bool flag = 0;
-    double ans = 1e18;
-    if (!judge(P[1],P[2],P[3])) {
-    ans = area(P[1],P[2],P[3]);
-        flag = 1;
+long double cross(struct poi a,struct poi b) {
+    return a.x * b.y - a.y * b.x; 
+}
+long double mul(struct poi a,struct poi b) {
+    return a.x * b.x + a.y * b.y; 
+}
+
+long double len(struct poi a) {
+    return sqrt(mul(a,a));
+}
+
+struct poi del(struct poi a,struct poi b) {
+    struct poi c;
+    c.x = a.x - b.x;
+    c.y = a.y - b.y;
+    return c;
+}
+
+int judge(struct poi a,struct poi b,struct poi c) {
+    return !sgn(cross(del(b,a),del(c,b)));
+}
+long double area(struct poi a,struct poi b,struct poi c) {
+    return (long double)fabs(cross(del(b,a),del(c,a))) / 2.0;
+}
+
+int main() {
+    scanf("%Lf %Lf %Lf %Lf %Lf %Lf",&p[0].x,&p[0].y,&p[1].x,&p[1].y,&p[2].x,&p[2].y);
+    if (!judge(p[0],p[1],p[2])) {
+        long double s = area(p[0],p[1],p[2]);
+        long double d = len(del(p[0],p[1])) + len(del(p[0],p[2])) + len(del(p[1],p[2]));
+        printf("L = %.2Lf, A = %.2Lf\n",d,s);
+    } else {
+        puts("Impossible");
     }
-
-    double L = len(P[1]-P[2]) + len(P[2]-P[3]) + len(P[1] - P[3]);
-    if (!flag) cout << "Impossible\n";
-    else cout << fixed << setprecision(2) << "L = " << L << ", A = " << ans << "\n";
-} 
-
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);//cout.tie(0);
-    solve();
     return 0;
 }

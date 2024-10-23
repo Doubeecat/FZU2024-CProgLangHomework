f9b070b8474d9736a210c1c7fdf9fb27816b1f1e
Doubeecat <1359446223@qq.com>
2024-10-23 20:38:05 +0800

    fix the typo of 7-2

--- 【实践课内】3.选择结构2/7-2.c
+++ 【实践课内】3.选择结构2/7-2.c
@@ -7,7 +7,7 @@ double a[10];
 int main() {
     scanf("%d\n",&typ);
     for (int i = 0;i < 3;++i) scanf("%lf ",&a[i]); 
-    if (typ) {
+    if (!typ) {
         for (int i = 0;i < 3;++i) {
             for (int j = i + 1;j < 3;++j) {
                 if (a[i] > a[j]) {
@@ -29,6 +29,6 @@ int main() {
         }
 
     }
-    for (int i = 0;i < 3;++i) printf("%.2lf ",a[i]);
+    printf("%.2lf %.2lf %.2lf\n",a[0],a[1],a[2]);
     return 0;
 }
